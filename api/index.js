6da27ba9f2ba9e79848c92f3f06332058e716500
Martín Ponce <martinp88@gmail.com>
2026-07-05 11:47:45 -0300

    add copilot fix

--- api/index.js
+++ api/index.js
@@ -34,8 +34,10 @@ app.use((err, req, res, next) => {
   res.status(500).send('Algo salió mal en el servidor');
 });
 
-app.listen(3000, () => {
-  console.log('Servidor escuchando en http://localhost:3000');
-});
+if (process.env.NODE_ENV !== 'production' && !process.env.VERCEL) {
+  app.listen(3000, () => {
+    console.log('Servidor escuchando en http://localhost:3000');
+  });
+}
 
 export default app;
