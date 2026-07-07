b79b882e29d3cac7ffb59190960c7e682117c3b6
Martín Ponce <martinp88@gmail.com>
2026-07-07 14:36:22 -0300

    clean LiveStatus component

--- client/src/components/LiveStatus.vue
+++ client/src/components/LiveStatus.vue
@@ -11,7 +11,7 @@ const API_URL =
 const checkStatus = async () => {
   try {
     const res = await axios.get(API_URL)
-    isLive.value = true //res.data.isLive
+    isLive.value = res.data.isLive
   } catch (err) {
     isLive.value = false
   }
@@ -55,26 +55,4 @@ onMounted(() => {
       <i class="fas fa-external-link-alt text-[10px]"></i>
     </a>
   </div>
-  <!--
-  <div
-    class="flex items-center space-x-2 font-mono text-sm md:text-lg font-bold tracking-wider uppercase"
-  >
-    <template v-if="isLive">
-      <span class="relative flex h-3 w-3">
-        <span
-          class="animate-ping absolute inline-flex h-full w-full rounded-full bg-emerald-400 opacity-75"
-        ></span>
-        <span
-          class="relative inline-flex rounded-full h-3 w-3 bg-emerald-500 shadow-[0_0_10px_#10b981]"
-        ></span>
-      </span>
-      <span class="text-emerald-400 drop-shadow-[0_0_6px_#10b981]">LIVE</span>
-    </template>
-
-    <template v-else>
-      <span class="h-3 w-3 rounded-full bg-rose-950 border border-rose-800"></span>
-      <span class="text-rose-700/60 drop-shadow-[0_0_4px_rgba(244,63,94,0.1)]">OFFLINE</span>
-    </template>
-  </div>
-  -->
 </template>
